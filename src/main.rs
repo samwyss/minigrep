45c64f58649c53349b4a2c5e689eebedb7c507f9
Sam Wyss <wysss@purdue.edu>
2023-07-02 21:27:35 -0400

    added error handling on run

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,18 +10,16 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments {}", err);
         process::exit(1);
-    });
+    }); //use pattern for error handling code that has a return value
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application Error {}", e);
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file ");
-
-    println!("With text: \n{}", contents);
+        process::exit(1);
+    } //use pattern when error handling code that is only ran for side effects
 }
 
 struct Config {
@@ -39,4 +38,15 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text: \n{}", contents);
+
+    Ok(())
 }
