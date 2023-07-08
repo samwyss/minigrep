537b89509fdccda586c1ade8462b1a9f8d62d33d
Sam Wyss <wysss@purdue.edu>
2023-07-07 22:16:12 -0400

    Finished Ch 13 Example

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,41 @@
+use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough input arguments")
+            return Err("not enough input arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = match env::var("CASE_SENSITIVE") {
+            Ok(val) => {
+                if val == "True" {
+                    true
+                } else if val == "False" {
+                    false
+                } else {
+                    true // defaults true if $env::CASE_SENSITIVE != $true or $env::CASE_SENSITIVE != $false
+                }
+            }
+            Err(_e) => true,
+        };
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -26,7 +45,69 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text: \n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line)
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct Tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments {}", err);
+        eprintln!("Problem parsing arguments {}", err); //eprintln!() prints to standard error
         process::exit(1);
     }); //use pattern for error handling code that has a return value
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error {}", e);
+        eprintln!("Application Error {}", e); //eprintln!() prints to standard error
 
         process::exit(1);
     } //use pattern when error handling code that is only ran for side effects
