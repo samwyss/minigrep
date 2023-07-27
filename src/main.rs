fb76476c3de614604b6b0bd91fd94f49cad09f57
sjwys <sjwyss@outlook.com>
2023-07-26 21:34:22 -0400

    Modified code to use iterators instead of loops

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments {}", err); //eprintln!() prints to standard error
         process::exit(1);
     }); //use pattern for error handling code that has a return value
